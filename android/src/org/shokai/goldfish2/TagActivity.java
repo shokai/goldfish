84d0f51a76a32555506b1e90c167472820d72e77
Sho Hashimoto <hashimoto@shokai.org>
2011-10-03 17:04:51 +0900

    tag activity

---
+++ android/src/org/shokai/goldfish2/TagActivity.java
@@ -0,0 +1,13 @@
+package org.shokai.goldfish2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TagActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tag);
+    }
+}

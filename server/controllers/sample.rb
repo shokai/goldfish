3fb93c1d98dcad543b55ffe3019d70a24e0e3372
Sho Hashimoto <hashimoto@shokai.org>
2011-11-19 18:11:18 +0900

    rename views

--- server/controllers/sample.rb
+++ server/controllers/sample.rb
@@ -6,26 +6,26 @@ end
 
 get '/samples/orientation' do
   @tag = 'samples/orientation'
-  haml 'samples/orientation'.to_sym
+  haml :samples_orientation
 end
 
 get '/samples/tcp_chat' do
   @host = env['SERVER_NAME']
   @port = @@conf['chat_port']
   @tag = 'samples/tcp_chat'
-  haml 'samples/tcp_chat'.to_sym
+  haml :samples_tcp_chat
 end
 
 get '/samples/udp_mouse' do
   @host = env['SERVER_NAME']
   @port = @@conf['mouse_port']
   @tag = 'samples/udp_mouse'
-  haml 'samples/udp_mouse'.to_sym
+  haml :samples_udp_mouse
 end
 
 get '/samples/udp_touchpad' do
   @host = env['SERVER_NAME']
   @port = @@conf['mouse_port']
   @tag = 'samples/udp_touchpad'
-  haml 'samples/udp_touchpad'.to_sym
+  haml :samples_udp_touchpad
 end

62418a6e2847b7f9ea14e2b559589cbf7ea0548f
Sho Hashimoto <hashimoto@shokai.org>
2011-11-19 18:22:59 +0900

    tag saves url

--- server/bootstrap.rb
+++ server/bootstrap.rb
@@ -3,18 +3,15 @@ require 'bundler/setup'
 require 'rack'
 require 'sinatra/reloader' if development?
 require 'mongoid'
+require 'haml'
+require 'yaml'
+require 'json'
 [:models ,:controllers].each do |dir|
   Dir.glob(File.dirname(__FILE__)+"/#{dir}/*.rb").each do |rb|
     puts "loading #{rb}"
     require rb
   end
 end
-# require File.dirname(__FILE__)+'/models/nfctag'
-# require File.dirname(__FILE__)+'/controllers/sample.rb'
-require 'haml'
-require 'yaml'
-require 'json'
-
 
 begin
   @@conf = YAML::load open(File.dirname(__FILE__)+'/config.yaml').read
--- server/models/nfctag.rb
+++ server/models/nfctag.rb
@@ -2,11 +2,11 @@
 class NfcTag
   include Mongoid::Document
   field :hex_id, :type => String, :default => ''
-  field :script, :type => String, :default => ''
+  field :url, :type => String, :default => ''
   def to_hash
     {
       :hex_id => id,
-      :script => script
+      :url, :type => String
     }
   end
 end

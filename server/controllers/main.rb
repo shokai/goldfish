61a5cce0d1547bf03bda3e793225209442df0673
Sho Hashimoto <hashimoto@shokai.org>
2011-11-27 18:44:03 +0900

    remove unused files

--- server/controllers/main.rb
+++ server/controllers/main.rb
@@ -5,8 +5,3 @@ end
 get '/' do
   haml :index
 end
-
-get '/t/:tag' do
-  @tag = params[:tag]
-  haml :tag
-end
--- server/public/js/tag.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-      console.log("start - tag:"+gynamic.tag());
-      alert("app_name:"+gynamic.app_name());
-  });
